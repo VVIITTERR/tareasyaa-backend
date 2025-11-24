20158ce8241083b21a70f6203d74fde8e4d15ee9
VVITTERR <omonterios15@gmail.com>
2025-11-24 02:16:28 -0500

    Fix: Disable SSL redirect for Railway

--- tareasyaa/settings.py
+++ tareasyaa/settings.py
@@ -131,7 +131,8 @@ REST_FRAMEWORK = {
 
 # Security settings for production
 if not DEBUG:
-    SECURE_SSL_REDIRECT = True
+    SECURE_PROXY_SSL_HEADER = ('HTTP_X_FORWARDED_PROTO', 'https')
+    SECURE_SSL_REDIRECT = False  # False para Railway
     SESSION_COOKIE_SECURE = True
     CSRF_COOKIE_SECURE = True
     SECURE_BROWSER_XSS_FILTER = True
